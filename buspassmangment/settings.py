cf587c5c68c11836cd4fce0c3c04859ab0062a16
Uday061103 <136961821+Uday061103@users.noreply.github.com>
2025-03-05 22:51:19 +0530

    fixed css issue

--- buspassmangment/settings.py
+++ buspassmangment/settings.py
@@ -119,6 +119,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')]
 
 # Default primary key field type
