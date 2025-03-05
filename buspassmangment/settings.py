c6d17963b681471780b470b6f87d16a70dd90873
Uday061103 <136961821+Uday061103@users.noreply.github.com>
2025-03-05 22:34:58 +0530

    Update settings.py

--- buspassmangment/settings.py
+++ buspassmangment/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4v4f3#_ua*^n%xfi&3=i_42c+wx=pduym9#un90$qa^nnz+k^4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
